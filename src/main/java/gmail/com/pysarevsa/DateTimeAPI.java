b20e8d7cc0655e0151fe588961869ecebf663820
brunnen-g <qazabarovsky@gmail.com>
2025-01-25 18:52:04 +0200

    Remove some spaces and blank lines

--- src/main/java/gmail/com/pysarevsa/DateTimeAPI.java
+++ src/main/java/gmail/com/pysarevsa/DateTimeAPI.java
@@ -6,31 +6,27 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 
+public class DateTimeAPI {
 
-    public class DateTimeAPI {
+    public static String getDateTimeFromAPI(String apiUrl) throws Exception {
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
+        connection.setRequestMethod("GET");
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
 
-        public static String getDateTimeFromAPI(String apiUrl) throws Exception {
-            URL url = new URL(apiUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            StringBuilder response = new StringBuilder();
+            String line;
 
-
-            connection.setRequestMethod("GET");
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
-
-
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                StringBuilder response = new StringBuilder();
-                String line;
-
-                while ((line = in.readLine()) != null) {
-                    response.append(line);
-                }
-                System.out.println(response.toString());
-                return response.toString();
+            while ((line = in.readLine()) != null) {
+                response.append(line);
             }
+            System.out.println(response.toString());
+            return response.toString();
         }
     }
+}
 
 
--- src/main/java/gmail/com/pysarevsa/Main.java
+++ src/main/java/gmail/com/pysarevsa/Main.java
@@ -1,31 +1,21 @@
 package gmail.com.pysarevsa;
 
-
-
 import static gmail.com.pysarevsa.DateTimeAPI.getDateTimeFromAPI;
-
 
 public class Main {
 
-
     public static void main(String[] args) {
-
         String apiUrl = "http://worldtimeapi.org/api/timezone/Etc/UTC";
 
         try {
-
-            String response = getDateTimeFromAPI(apiUrl); //отвер апи
-
-
-
-
+            String response = getDateTimeFromAPI(apiUrl);
 
             int dateTimeIndex = response.indexOf("\"datetime\":\"");
             if (dateTimeIndex != -1) {
                 int start = dateTimeIndex + 12;
                 int end = response.indexOf('T', start);
                 int tstart = dateTimeIndex + 23;
-                int tend= response.indexOf('.', tstart);
+                int tend = response.indexOf('.', tstart);
                 String dateTime = response.substring(start, end);
                 String dateTimevalue = response.substring(tstart, tend);
                 System.out.println("Точная дата: " + dateTime);
@@ -38,8 +28,5 @@ public class Main {
             System.err.println("Ошибка при получении данных бля: " + e.getMessage());
         }
 
-
     }
 }
-
-
