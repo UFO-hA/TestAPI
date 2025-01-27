70b2c733e97b95ff5c8059fca1c1a322b4775583
Serhii Pysarev <pysarevsa@gmail.com>
2025-01-27 16:17:11 +0100

    Start API add new API and key

--- src/main/java/gmail/com/pysarevsa/Main.java
+++ src/main/java/gmail/com/pysarevsa/Main.java
@@ -1,32 +1,30 @@
 package gmail.com.pysarevsa;
-
-import static gmail.com.pysarevsa.DateTimeAPI.getDateTimeFromAPI;
+import static gmail.com.pysarevsa.DateTimeAPI.getDateTime;
 
 public class Main {
-
     public static void main(String[] args) {
-        String apiUrl = "http://worldtimeapi.org/api/timezone/Etc/UTC";
-
+        UrlApi UrlApi = new UrlApi("http://api.timezonedb.com/v2.1/get-time-zone?key=4ESRG1IH1NNB&format=json&by=zone&zone=Europe/Berlin"); //Новый API с ключём. Пока только для региона Europe/Berlin
+        /* В дальнейшем планирую добавить ключь отдельным файлом и возможность настройки
+        * региона. У данного API есть функция определения местонахождения по ip.
+        * но эта опция платная */
+        String url = UrlApi.getUrl();
         try {
-            String response = getDateTimeFromAPI(apiUrl);
-
-            int dateTimeIndex = response.indexOf("\"datetime\":\"");
+            String response = getDateTime(url);
+            int dateTimeIndex = response.indexOf("\"formatted\":");
             if (dateTimeIndex != -1) {
-                int start = dateTimeIndex + 12;
-                int end = response.indexOf('T', start);
-                int tstart = dateTimeIndex + 23;
-                int tend = response.indexOf('.', tstart);
-                String dateTime = response.substring(start, end);
-                String dateTimevalue = response.substring(tstart, tend);
-                System.out.println("Точная дата: " + dateTime);
-                System.out.println("Точное время: " + dateTimevalue);
+                int timeStart = dateTimeIndex + 25;
+                int timeEnd = response.indexOf('\"', timeStart);
+                int dateStart = dateTimeIndex + 14;
+                int dateEnd = response.indexOf(' ', dateStart);
+                String Date = response.substring(dateStart, dateEnd);
+                String Time = response.substring(timeStart, timeEnd);
+                System.out.println("Точное время: " + Time);
+                System.out.println("Точная дата: " + Date);
             } else {
-                System.out.println("Поле datetime не найдено в ответе API.");
+                System.out.println("Поле formatted не найдено в ответе API.");
             }
-
         } catch (Exception e) {
-            System.err.println("Ошибка при получении данных бля: " + e.getMessage());
+            System.err.println("Ошибка при получении данных: " + e.getMessage());
         }
-
     }
 }
---
+++ src/main/java/gmail/com/pysarevsa/UrlApi.java
@@ -0,0 +1,16 @@
+package gmail.com.pysarevsa;
+
+public class UrlApi {
+    private String url;
+
+    public UrlApi(String url){
+        this.url = url;
+    }
+    public String getUrl(){
+        return url;
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+}
