0f753ce958737a2de3c75ee4f792b2f37a29d92c
Serhii Pysarev <pysarevsa@gmail.com>
2025-01-30 16:34:42 +0100

    ubral musor

--- src/main/java/gmail/com/pysarevsa/Main.java
+++ src/main/java/gmail/com/pysarevsa/Main.java
@@ -2,17 +2,18 @@ package gmail.com.pysarevsa;
 
 
 public class Main {
+
     public static void main(String[] args) {
+
+
         String url = ("http://api.timezonedb.com/v2.1/get-time-zone?key=4ESRG1IH1NNB&format=json&by=zone&zone=Europe/Berlin");
 
         try {
             DateTimeApiClient api = new DateTimeApiClient(url);
             DateTimeModel response = api.getDateTime();
-            String Date = response.getFormatted().substring(0, 10);
-            String Time = response.getFormatted().substring(11, 19);
             System.out.println("Точное время и дата: " + response.getFormatted());
-            System.out.println("Точное время: " + Time);
-            System.out.println("Точная дата: " + Date);
+            System.out.println("Точное время: " + response.getFormatted().substring(11, 19));
+            System.out.println("Точная дата: " + response.getFormatted().substring(0, 10));
             System.out.println("Страна: " + response.getCountryName());
             System.out.println("Часовой пояс: " + response.getZoneName());
         } catch (Exception e) {
