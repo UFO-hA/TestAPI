9b0b500d77989a0cd89ec66c03ceac2297caa7de
Serhii Pysarev <pysarevsa@gmail.com>
2025-02-03 19:51:31 +0100

    add class RequestConfig

--- src/main/java/gmail/com/pysarevsa/Main.java
+++ src/main/java/gmail/com/pysarevsa/Main.java
@@ -1,26 +1,30 @@
 package gmail.com.pysarevsa;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-
-
-        String url = ("http://api.timezonedb.com/v2.1/get-time-zone?key=4ESRG1IH1NNB&format=json&by=zone&zone=Europe/Berlin");
-
+        RequestConfig config = new RequestConfig();
+        config.setUri("http://api.timezonedb.com/v2.1/get-time-zone");
+        Map<String, String> params = new HashMap<>();
+        params.put("key", "4ESRG1IH1NNB");
+        params.put("format", "json");
+        params.put("by", "zone");
+        params.put("zone", "Europe/Berlin");
+        config.setQueryParams(params);
         try {
-            DateTimeApiClient api = new DateTimeApiClient(url);
+            DateTimeApiClient api = new DateTimeApiClient(config);
             DateTimeModel response = api.getDateTime();
-            //System.out.println("Точное время и дата: " + response.getFormatted());
+            System.out.println("Точное время и дата: " + response.getFormatted());
             System.out.println("Точное время: " + response.getFormatted().substring(11, 19));
             System.out.println("Точная дата: " + response.getFormatted().substring(0, 10));
             System.out.println("Страна: " + response.getCountryName());
             System.out.println("Часовой пояс: " + response.getZoneName());
-
-
         } catch (Exception e) {
             System.err.println("Ошибка при получении данных: " + e.getMessage());
         }
-
     }
 }
--- src/main/java/gmail/com/pysarevsa/RequestConfig.java
+++ src/main/java/gmail/com/pysarevsa/RequestConfig.java
@@ -1,4 +1,26 @@
 package gmail.com.pysarevsa;
 
+import java.util.Map;
+
 public class RequestConfig {
+    private Map<String, String> queryParams;
+    private String uri;
+
+    public Map<String, String> getQueryParams() {
+        return queryParams;
+    }
+
+    public void setQueryParams(Map<String, String> queryParams) {
+        this.queryParams = queryParams;
+    }
+
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
 }
+
